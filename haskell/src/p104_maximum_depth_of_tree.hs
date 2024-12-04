8eef94b38913b3a14beddc58cb75e9f06439ed07
Yuta Nakamura <nakamurayuta0204@gmail.com>
2024-12-05 01:14:35 +0900

    fix: warning

--- haskell/src/p104_maximum_depth_of_tree.hs
+++
@@ -1,6 +0,0 @@
-data TreeNode = Node TreeNode TreeNode Int | Edge Int deriving (Show)
-
-maxDepth :: TreeNode -> Int
-maxDepth x = case x of
-  Node left right _ -> max (maxDepth left) (maxDepth right) + 1
-  Edge _ -> 1
