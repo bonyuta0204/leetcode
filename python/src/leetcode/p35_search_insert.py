6d3ad0a923f87217792b8f27033b899a24261d22
Yuta Nakamura <nakamurayuta0204@gmail.com>
2024-12-08 22:35:40 +0900

    binary search

--- python/src/leetcode/p35_search_insert.py
+++ python/src/leetcode/p35_search_insert.py
@@ -1,37 +1,17 @@
 from typing import List
 
+
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
 
-        if len(nums) == 0:
-            return 0
-
-        left = 0
-        right = len(nums) - 1
-
-        if nums[right] < target:
-            return right + 1
-
-        if nums[right] == target:
-            return right 
-
-        if nums[0] > target:
-            return 0
-
-        while True:
+        while left <= right:
             mid = (left + right) // 2
 
-            if left == right -1 and nums[left] != target:
-                return right
-
-
-            if nums[mid] < target:
-                left = mid
-
-            elif nums[mid] > target:
-                right = mid
-            else:
+            if nums[mid] == target:
                 return mid
-
-
-
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left
