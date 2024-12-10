40e2f8fd45b5fa1ad0723d478849a6ca61310b6f
Yuta Nakamura <nakamurayuta0204@gmail.com>
2024-12-11 00:00:54 +0900

    p46 permutations

---
+++ python/src/leetcode/p46_permutations.py
@@ -0,0 +1,31 @@
+from typing import List
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        """
+        we can use permutation for N length list from N-1 length list.
+        N length permutation can by created by N times by inserting new element in all possible position.
+
+        We can solve it resursively but,  we use iterative approach to avoid using too much stack memory
+        """
+
+        if len(nums) == 0:
+            return [[]]
+
+        result = [[]]
+
+        for n in nums:
+            new_result = []
+            for r in result:
+                # e.g r = [1, 2, 3]
+                for p in range(len(r) + 1):
+                    new_list = r.copy()
+                    new_list.insert(p, n)
+                    new_result.append(new_list)
+            result = new_result
+
+        return result
+
+
+
+
