4990a33d3e8c9081c4032bbe9c6b057e4fdf6732
Yuta Nakamura <nakamurayuta0204@gmail.com>
2024-12-15 15:14:23 +0900

    p53_maximum_subarray fixed

--- python/src/leetcode/p53_maximum_subarray.py
+++ python/src/leetcode/p53_maximum_subarray.py
@@ -3,21 +3,18 @@ from typing import List
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         """
-        We can know the answer at n-th if we know the answer for n - 1.
-        the largest maxSubArray which end at poistion n is either Result(n-1) + n or N
+        We have to track two maximums.
+        - current_maximum
+          - maximum sum for subArray ending at this position
+        - global_maximum
+          - global maximum for ending at position prior to current position
         """
-        highest_num = [0 for _ in nums]
 
-        if len(nums) == 0:
-            return 0
 
-        for i, n in enumerate(nums):
-            if i == 0:
-                highest_num[i] = n
-            else:
-                last_highest = highest_num[i-1]
-                if last_highest > 0:
-                    highest_num[i] = n + last_highest
-                else:
-                    highest_num[i] = n
-        return max(highest_num)
+        current_maximum = nums[0]
+        global_maximum = nums[0]
+        for n in nums[1:]:
+            current_maximum = max(n, n + current_maximum)
+            global_maximum = max(global_maximum, current_maximum)
+        return global_maximum
+
