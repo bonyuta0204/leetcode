ee9f75e62a4afdad0ac883318c017b63e3ae2d56
Yuta Nakamura <nakamurayuta0204@gmail.com>
2025-01-18 15:20:10 +0900

    p153 find min

--- python/src/leetcode/p153_findMin.py
+++ python/src/leetcode/p153_findMin.py
@@ -9,28 +9,12 @@ class Solution:
         start = 0
         end = len(nums) - 1
 
-        def half_index(start, end):
-            if start < end:
-                return (start + end) // 2
-            else:
-                idx = (start + end + len(nums)) // 2
-                if idx >= len(nums):
-                    idx -= len(nums)
-                return idx
+        while start < end:
+            mid = (start + end) // 2
 
-        while start != end:
-            # minimum should be somewhere between start and end
-            if nums[start] == nums[end]:
-                return nums[start]
-            next_half = half_index(start, end)
-            # reach to the end
-            if next_half == start or next_half == end:
-                return min(nums[start], nums[end])
-
-            if nums[start] > nums[next_half]:
-                end = next_half
-            elif nums[next_half] > nums[end]:
-                start = next_half
+            if nums[mid] > nums[end]:
+                start = mid + 1
             else:
-                return nums[start]
-        return nums[start]
+                end = mid
+        return min(nums[start], nums[end])
+
